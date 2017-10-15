85dca34b2978b5ef60a0c4fa952db24f890e8d6c
Patrick Nguyen <superpatricko@gmail.com>
2017-10-15 17:06:11 -0400

    Add node-dir library and diff folders

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,43 +1,78 @@
 var async = require('async');
 var fs = require('fs');
-
-// Pretty diff: https://github.com/prettydiff/prettydiff/
+var path = require('path');
+var dir = require('node-dir');
 var jsdiff = require('diff');
 
 exports.index = function(req, res) {
-	async.parallel({
-		one: function(cb) {
-			fs.readFile('./zip/DEV/file1.txt', 'utf8', function read(err, data) {
-				if (err) {
-					cb(err);
+	var devDirectory = "./zip/DEV";
+	var totaldiff = '';
+
+	// Get all the paths
+	dir.paths(devDirectory, true, function(err, paths) {
+		if (err) throw err;
+
+		// Iterate through paths
+		async.eachSeries(paths, function(sPath, pathsCb) {
+			// Keep only file paths
+			fs.stat(sPath, function(err, stats) {
+				if (stats.isFile()) {
+					// Read and compare paths
+					async.parallel({
+						DEV: function(devCb) {
+							fs.readFile(sPath, 'utf8', function read(err, data) {
+								if (err) {
+									devCb(err);
+									return;
+								}
+
+								devCb(null, data);
+							});
+						},
+						QA: function(qaCb) {
+							var qaPath = sPath.replace(/DEV/g, 'QA');
+							fs.readFile(qaPath, 'utf8', function read(err, data) {
+								if (err && err.code === 'ENOENT') {
+									totaldiff += "FILE NOT FOUND IN QA: " + err.path + "\n";
+									qaCb("ENOENT");
+									return;
+								}
+
+								qaCb(null, data);
+							})
+						}
+					}, function(err, results) {
+						// Compare and update the totaldiff
+
+						// ENOENT isn't an actual error in our case
+						if (err === "ENOENT") {
+							pathsCb();
+							return;
+						}
+
+						if (err) {
+							pathsCb(err);
+							return;
+						}
+
+						totaldiff += jsdiff.createTwoFilesPatch(sPath, sPath.replace(/DEV/g, 'QA'), results.DEV, results.QA) + "\n";
+						pathsCb();
+					})
+				} else {
+					// have to call back if it's a directory
+					pathsCb();
 				}
-				cb(null, data);
-			});
+			})
 
-		},
-		two: function(cb) {
-			fs.readFile('./zip/QA/file1.txt', 'utf8', function read(err, data) {
-				if (err) {
-					cb(err)
-				}
-				cb(null, data);
-			});
-		}
-	}, function(err, results) {
-		if (err) {
-			res.json(err);
-			return;
-		}
+		}, function(err) {
+			if (err) {
+				console.log(err);
+				res.json(err);
+				return;
+			}
 
-		var foo = jsdiff.createTwoFilesPatch("file1.txt", "file1.txt", results.one, results.two, "timestamp1", "timestamp2");
-		console.log(JSON.stringify(foo)); // to show escape characters
-
-		// text/plain to show linebreaks (i.e: \n)
-		res.set('Content-Type', 'text/plain');
-		res.send(foo);
+			res.set('Content-Type', 'text/plain');
+			res.send(totaldiff);
+		})
 	})
-
-	// 1. Diff a pair of files (DONE)
-	// 2. Diff two pairs of files
-	// 3. Unzip and compare the folders
 };
