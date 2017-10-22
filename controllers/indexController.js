946d42231c24c1ffb44e75edbb16997130c6e118
Patrick Nguyen <superpatricko@gmail.com>
2017-10-22 17:07:31 -0400

    Make diff2html work

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,78 +1,29 @@
-var async = require('async');
+var diff2html = require("diff2html").Diff2Html
 var fs = require('fs');
-var path = require('path');
-var dir = require('node-dir');
-var jsdiff = require('diff');
+var exec = require('child_process').exec;
+
+
 
 exports.index = function(req, res) {
-	var devDirectory = "./zip/DEV";
-	var totaldiff = '';
+	// res.set('Content-Type', 'text/plain');
+	exec("diff -aru zip\\DEVtest zip\\QAtest", function (err, stdout, stderr) {
+		if (err) {
+			console.log(err);
+		}
+		res.json(diff2html.getJsonFromDiff(stdout));
+	});
 
-	// Get all the paths
-	dir.paths(devDirectory, true, function(err, paths) {
-		if (err) throw err;
 
-		// Iterate through paths
-		async.eachSeries(paths, function(sPath, pathsCb) {
-			// Keep only file paths
-			fs.stat(sPath, function(err, stats) {
-				if (stats.isFile()) {
-					// Read and compare paths
-					async.parallel({
-						DEV: function(devCb) {
-							fs.readFile(sPath, 'utf8', function read(err, data) {
-								if (err) {
-									devCb(err);
-									return;
-								}
+	// fs.readFile("./zip/test4WithUnifiedNodeDiff", 'utf8', function read(err, data) {
+	// 	if (err) {
+	// 		return;
+	// 	}
+	// 	res.json(diff2html.getJsonFromDiff(data));
 
-								devCb(null, data);
-							});
-						},
-						QA: function(qaCb) {
-							var qaPath = sPath.replace(/DEV/g, 'QA');
-							fs.readFile(qaPath, 'utf8', function read(err, data) {
-								if (err && err.code === 'ENOENT') {
-									totaldiff += "FILE NOT FOUND IN QA: " + err.path + "\n";
-									qaCb("ENOENT");
-									return;
-								}
+	// 	// console.log(diff2html.getJsonFromDiff(data));
+	// });
 
-								qaCb(null, data);
-							})
-						}
-					}, function(err, results) {
-						// Compare and update the totaldiff
 
-						// ENOENT isn't an actual error in our case
-						if (err === "ENOENT") {
-							pathsCb();
-							return;
-						}
-
-						if (err) {
-							pathsCb(err);
-							return;
-						}
-
-						totaldiff += jsdiff.createTwoFilesPatch(sPath.replace(/zip\\DEV/, ''), sPath.replace(/zip\\DEV/, ''), results.DEV, results.QA) + "\n";
-						pathsCb();
-					})
-				} else {
-					// have to call back if it's a directory
-					pathsCb();
-				}
-			})
-
-		}, function(err) {
-			if (err) {
-				console.log(err);
-				res.json(err);
-				return;
-			}
-
-			res.set('Content-Type', 'text/plain');
-			res.send(totaldiff);
-		})
-	})
+	// res.set('Content-Type', 'text/plain');
+	// res.send(totaldiff);
 };
