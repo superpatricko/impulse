0f2c9a94e34117abf73558373f3d9bc081b1d500
Patrick Nguyen <superpatricko@gmail.com>
2017-10-27 18:04:27 -0400

    remove unnecessary files

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,19 +1,29 @@
-var diff2html = require("diff2html").Diff2Html
 var fs = require('fs');
 var exec = require('child_process').exec;
 
 
 
 exports.index = function(req, res) {
-	// res.render('index', { title: 'Local Library Home' });
 
-	// res.set('Content-Type', 'text/plain');
+	res.set('Content-Type', 'text/plain');
 	// 1024 = 1KB
-	exec("diff -aru zip\\DEVtest zip\\QAtest", {maxBuffer: 1024 * 100000}, function (err, stdout, stderr) {
-		// res.send(diff2html.getPrettyHtml(stdout));
-		res.render('index', { title: 'diffoo', content: diff2html.getPrettyHtml(stdout)})
+	exec("diff -aru zip\\DEV zip\\QA", {maxBuffer: 1024 * 100000}, function (err, stdout, stderr) {
+		stdout.toString();
 	});
 
+	// 1. unzip DEV and QA into git folder
+
+
+	// ALL GIT COMMANDS WILL BE IN A .gitignored FOLDER and the cwd will be the git thing
+	// git checkout -B dev
+	// git add DEV
+	// git commit -m "dev commit"
+	// git push -u origin dev
+
+	// git checkout -B qa
+	// git add QA
+	// git commit -m "qa commit"
+	// git push -u origin qa
 
 	// fs.readFile("./zip/test4WithUnifiedNodeDiff", 'utf8', function read(err, data) {
 	// 	if (err) {
