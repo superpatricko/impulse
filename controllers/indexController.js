89818094718da0bb373e5094fe3f4784c84ba33e
Patrick Nguyen <superpatricko@gmail.com>
2017-10-27 19:53:25 -0400

    basic functionality

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var exec = require('child_process').exec;
+var async = require('async');
 
 var AdmZip = require('adm-zip');
 const git = require('simple-git')('./zip');
@@ -11,19 +12,22 @@ const remote = `https://${USER}:${PASS}@${REPO}`;
 exports.index = function(req, res) {
 	var DEVzip = new AdmZip("./zip/DEV.zip");
 	var QAzip = new AdmZip("./zip/QA.zip");
+	console.log(DEVzip);
+	res.send('foo');
 
 	// DEVzip.extractAllTo(/*target path*/"./zip/content", /*overwrite*/true)
 	async.series({
 		zero: function(cb) {
-			git.raw([
-				'remote',
-				'add',
-				'origin',
-				remote
-			], function() {
-				console.log(0);
-				cb()
-			});
+			// git.raw([
+			// 	'remote',
+			// 	'add',
+			// 	'origin',
+			// 	remote
+			// ], function() {
+			// 	console.log(0);
+				cb();
+			// });
+
 		},
 		one: function(cb) {
 			git.raw([
@@ -31,12 +35,15 @@ exports.index = function(req, res) {
 				'master'
 			], function() {
 				console.log(1);
-				cb()
+				cb();
 			});
 		},
 		oneandhalf: function(cb) {
-			DEVzip.extractAllTo( /*target path*/ "./zip/content", /*overwrite*/ true)
+			var devZipPath = './zip/.DRV_SAPIFRSDATAHUB-DEV.INTRANET.BELL.CA_00_D44_BC_EZ12598_(Default)/IFRS_R1'
+			// DEVzip.extractAllTo( /*target path*/ "./zip/content", /*overwrite*/ true)
+			DEVzip.extractEntryTo(devZipPath, "./zip/content", false, true);
 			console.log(1.5);
+			cb();
 		},
 		two: function(cb) {
 			git.raw([
@@ -45,7 +52,7 @@ exports.index = function(req, res) {
 				'DEV'
 			], function() {
 				console.log(2);
-				cb()
+				cb();
 			});
 		},
 		three: function(cb) {
@@ -54,7 +61,7 @@ exports.index = function(req, res) {
 				'content'
 			], function() {
 				console.log(3);
-				cb()
+				cb();
 			});
 		},
 		four: function(cb) {
@@ -64,7 +71,7 @@ exports.index = function(req, res) {
 				'DEV'
 			], function() {
 				console.log(4);
-				cb()
+				cb();
 			});
 		},
 		five: function(cb) {
@@ -75,66 +82,65 @@ exports.index = function(req, res) {
 				'DEV'
 			], function() {
 				console.log(5);
-				cb()
+				cb();
+			});
+		},
+		six: function (cb) {
+			git.raw([
+				'checkout',
+				'master'
+			], function() {
+				console.log(6);
+				cb();
+			});
+		},
+		sixandhalf: function(cb) {
+			var QAzipPath = './zip/.Q44_SAPIFRSDATAHUB-QA.INTRANET.BELL.CA_50_SINGLEDB_BC_EZ12598_(Default)/IFRS_R1'
+			QAzip.extractEntryTo(QAzipPath, "./zip/content", false, true);
+			console.log(6.5);
+			cb();
+		},
+		seven: function(cb) {
+			git.raw([
+				'checkout',
+				'-B',
+				'QA'
+			], function() {
+				console.log(7);
+				cb();
+			});
+		},
+		eight: function(cb) {
+			git.raw([
+				'add',
+				'content'
+			], function() {
+				console.log(8);
+				cb();
+			});
+		},
+		nine: function(cb) {
+			git.raw([
+				'commit',
+				'-m',
+				'QA'
+			], function() {
+				console.log(9);
+				cb();
+			});
+		},
+		ten: function(cb) {
+			git.raw([
+				'push',
+				'-uf',
+				'origin',
+				'QA'
+			], function() {
+				console.log(10);
+				cb();
 			});
 		}
 	}, function(err, results) {
-		res.send('woo');
+		res.render('index')
 	});
-
-	// Git.Repository.open('pathToRepo').then(function (repo) {
-	// 	// 1. git checkout master
-	// 	console.log(repo);
-	// 	repo.checkoutBranch("DEV").then(function () {
-
-	// 	});
-	// 	// 2. git checkout -B DEV
-
-	// 	// 3. git add content
-	// 	// 4. git commit -m "DEV"
-	// 	// 5. git push -u origin dev
-	// 	// 6. git checkout master
-
-	// 	// 7. unzip and repeat for QA
-
-	// })
-
-
-
-	// DEVzip.extractAllTo(/*target path*/"./zip/content", /*overwrite*/true)
-
-
-	// res.set('Content-Type', 'text/plain');
-	// res.send("woo");
-	// 1024 = 1KB
-	// exec("diff -aru zip\\DEV zip\\QA", {maxBuffer: 1024 * 100000}, function (err, stdout, stderr) {
-	// 	stdout.toString();
-	// });
-
-	// 1. unzip DEV and QA into git folder
-
-
-	// ALL GIT COMMANDS WILL BE IN A .gitignored FOLDER and the cwd will be the git thing
-	// git checkout -B dev
-	// git add DEV
-	// git commit -m "dev commit"
-	// git push -u origin dev
-
-	// git checkout -B qa
-	// git add QA
-	// git commit -m "qa commit"
-	// git push -u origin qa
-
-	// fs.readFile("./zip/test4WithUnifiedNodeDiff", 'utf8', function read(err, data) {
-	// 	if (err) {
-	// 		return;
-	// 	}
-	// 	res.json(diff2html.getJsonFromDiff(data));
-
-	// 	// console.log(diff2html.getJsonFromDiff(data));
-	// });
-
-
-	// res.set('Content-Type', 'text/plain');
-	// res.send(totaldiff);
 };
